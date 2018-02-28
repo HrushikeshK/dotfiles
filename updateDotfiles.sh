135087c4ed6a6b65be4299789bb7cf71346803bb
hrushikeshk <foo@nomail.notcom>
2018-02-28 23:49:40 +0530

    update updateDotfiles.sh

--- updateDotfiles.sh
+++ updateDotfiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dotdir="$HOME/dotfiles"
+dotdir="$HOME/.dotfiles"
 
 mkdir -p $HOME/.config/ranger && ln -sf $dotdir/.config/ranger/rc.conf $HOME/.config/ranger/rc.conf
 mkdir -p $HOME/.config/htop && ln -sf $dotdir/.config/htop/htoprc $HOME/.config/htop/htoprc
@@ -12,26 +12,26 @@ ln -sf $dotdir/.config/feh $HOME/.config/feh
 #ln -sf $dotdir/.config/mupen64plus $HOME/.config/mupen64plus
 ln -sf $dotdir/.config/compton.conf $HOME/.config/compton.conf
 ln -sf $dotdir/.gtkrc-2.0 $HOME/.gtkrc-2.0
-#ln -sf $dotdir/.zshrc $HOME/.zshrc
+ln -sf $dotdir/zsh/.zshrc $HOME/.zshrc
 ln -sf $dotdir/.Xresources $HOME/.Xresources
 #ln -sf $dotdir/.vimperatorrc $HOME/.vimperatorrc
 #ln -sf $dotdir/.vimperator/ $HOME/.vimperator
 
 # setup vim
-#ln -sf $dotdir/.vim/ $HOME/.vim
-#ln -sf $dotdir/.vimrc $HOME/.vimrc
+ln -sf $dotdir/vim/.vim/ $HOME/.vim
+ln -sf $dotdir/vim/.vimrc $HOME/.vimrc
 #mkdir -p $HOME/.vim/backups
-#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-#vim +PluginInstall +qall
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
 #python3 $dotdir/.vim/bundle/YouCompleteMe/install.py
 
 
 # setup tmux
-#ln -sf $dotdir/.tmux.conf $HOME/.tmux.conf 
-#mkdir -p $HOME/.tmux/plugins
-#if "test ! -d ~/.tmux/plugins/tpm" "run 'git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm'"
-#tmux source $HOME/.tmux.conf
-#~/.tmux/plugins/tpm/bin/install_plugins
+ln -sf $dotdir/tmux/.tmux.conf $HOME/.tmux.conf 
+mkdir -p $HOME/.tmux/plugins
+if "test ! -d ~/.tmux/plugins/tpm" "run 'git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm'"
+tmux source $HOME/.tmux.conf
+~/.tmux/plugins/tpm/bin/install_plugins
 
 # etc files
 #sudo mv /etc/lightdm/lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.old.conf
