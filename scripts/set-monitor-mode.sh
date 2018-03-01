019955e5b8187256516df18e03134d8a98529ead
hrushikeshk <foo@nomail.notcom>
2018-03-01 10:24:39 +0530

    Switching displaying modes using $mod+Insert is now possible

---
+++ scripts/set-monitor-mode.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+LAPTOP="eDP-1"
+HDMI="HDMI-1"
+
+MODE=$1
+
+if [ $# != 1 ]; then
+    echo "Usage: $0 [laptop, dual, clone]"
+fi
+
+if [ $1 == 'laptop' ]; then
+    xrandr --output $HDMI --off
+    xrandr --output $LAPTOP --primary --auto
+elif [ $1 == 'dual' ]; then
+    if (xrandr | grep "$LAPTOP connected" && xrandr | grep "$HDMI connected"); then
+	xrandr --output $HDMI --auto --right-of $LAPTOP
+	# Force work monitor into 1920x1080 mode
+	if (xrandr | grep "$HDMI connected 1440x900+1920+0"); then
+	    xrandr --output $HDMI --mode 1920x1080
+	fi
+    else
+	echo "No dual monitor detected. Exiting"
+	exit 0
+    fi
+elif [ $1 == 'clone' ]; then
+    xrandr --output $LAPTOP --auto --output $HDMI --auto --same-as $LAPTOP
+else
+    echo 'Invalid Input'
+fi
