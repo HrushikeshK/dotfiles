53639ad486eae85c87398327d9f8c9a33573a653
hrushikeshk <foo@nomail.notcom>
2018-03-01 12:39:31 +0530

    Now you can control spotify from your bar

--- scripts/spotify.sh
+++ scripts/spotify.sh
@@ -1,7 +1,37 @@
 #! /bin/bash
 
+if [ ! $(pidof spotify) ]; then
+    echo ' -'
+    exit 0
+fi
+
+artist=""
+
 title=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 1 "title"|egrep -v "title"|cut -b 44-|cut -d '"' -f 1|egrep -v ^$`
+
 if ! "$title" 2> /dev/null ; then
     artist=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 2 "artist"|egrep -v "artist"|egrep -v "array"|cut -b 27-|cut -d '"' -f 1|egrep -v ^$`
-    echo " " "$title" "-" "$artist";
+    echo -n " " "$title" "-" "$artist ";
 fi
+
+status=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'PlaybackStatus'|egrep -A 1 "string"|cut -b 26-|cut -d '"' -f 1|egrep -v ^$`
+
+case $status in 
+    Playing) echo " " ;;
+    Paused) echo " " ;;
+    Stopped) echo " " ;;
+esac
+
+case $BLOCK_BUTTON in 
+    # Play Pause
+    1) dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause ;;
+
+    # Stop
+    3) dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop ;;
+
+    # Next song (scroll up)
+    4) dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next ;; 
+
+    # Previous song (scroll down)
+    5) dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous ;;
+esac
