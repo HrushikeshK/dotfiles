e8b1d99b92082c69a30ab0f79e2045ffbd39c4ca
hrushikeshk <hru123.kakade@gmail.com>
2018-03-25 23:30:55 +0530

    Minor color/theme changes

--- .config/polybar/launch_polybar.sh
+++ .config/polybar/launch_polybar.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
     MONITOR=$m polybar -c ~/.dotfiles/.config/polybar/config --reload example &
